3c0be4243aa94020a2576ad27c10654e29b8ab99
Alcoholrithm <kmiiiaa@pusan.ac.kr>
2024-02-16 02:12:51 +0900

    Fix test/diabetes.py

--- test/diabetes.py
+++ test/diabetes.py
@@ -18,24 +18,6 @@ def load_diabetes():
     category_cols = []
     continuous_cols = list(map(str, data.columns))
 
-    for col in category_cols:
-        data[col] = le.fit_transform(data[col])
-
-    temp = None
-    for col in category_cols:
-        oh_values = OneHotEncoder().fit_transform(data[col].values.reshape((-1, 1))).toarray()
-        new_cols = [col + "-" + str(i) for i in range(len(data[col].unique()))]
-        oh_values = pd.DataFrame(oh_values, columns = new_cols, dtype=np.int8, index=data.index)
-        if temp is None:
-            temp = oh_values
-        else:
-            temp = temp.merge(oh_values, left_index=True, right_index=True)
-
-    data = data.merge(temp, left_index=True, right_index=True)
-    data.drop(category_cols, inplace=True, axis=1)
-
-    category_cols = temp.columns
-
     scaler = MinMaxScaler()
     data[continuous_cols] = scaler.fit_transform(data[continuous_cols])
     
