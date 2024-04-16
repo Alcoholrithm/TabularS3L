c23658892efcac6454bfbe8f454bec2c0450c7c7
Alcoholrithm <kmiiiaa@pusan.ac.kr>
2024-04-16 15:47:56 +0900

    Add a benchmark on cmc dataset and Update README.md

--- benchmark/datasets/__init__.py
+++ benchmark/datasets/__init__.py
@@ -1,4 +1,5 @@
 from .diabetes import load_diabetes
 from .abalone import load_abalone
+from .cmc import load_cmc
 
-__all__=["load_diabetes", "load_abalone"]
+__all__=["load_diabetes", "load_abalone", "load_cmc"]
