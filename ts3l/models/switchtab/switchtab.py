9a451c85011714c990d4d82fed54445727a3348f
Alcoholrithm <kmiiiaa@pusan.ac.kr>
2024-04-28 04:02:37 +0900

    Refactor SwitchTab nn.Module

--- ts3l/models/switchtab/switchtab.py
+++ ts3l/models/switchtab/switchtab.py
@@ -18,15 +18,8 @@ class Encoder(nn.Module):
         super().__init__()
         
         self.transformer = FTTransformer(cont_nums=cont_nums, cat_dims=category_dims, emb_dim=hidden_dim, n_heads=n_head, attn_dropout=dropout_rate, ffn_dropout=dropout_rate, ffn_factor_dim=ffn_factor, depth=encoder_depth)
-        # self.tokenizer = FeatureTokenizer(hidden_dim, cont_nums, category_dims)
-        # self.transformer = nn.TransformerEncoder(nn.TransformerEncoderLayer(d_model = hidden_dim, nhead = n_head, dim_feedforward=int(hidden_dim * ffn_factor), dropout=dropout_rate, batch_first=True), num_layers=encoder_depth)
-        # self.transformer = nn.Sequential(MLP(input_dim, hidden_dim, encoder_depth - 1, dropout_rate), nn.LeakyReLU(),                               nn.Linear(hidden_dim, input_dim))
-        # self.transformers = nn.Sequential(
-        #     *[nn.TransformerEncoderLayer(d_model = input_dim, nhead = n_head, dim_feedforward=hidden_dim, dropout=dropout_rate) for _ in range(encoder_depth)]
-        # )
         
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # x  = self.tokenizer(x)
         return self.transformer(x)
     
 class Projector(nn.Module):
@@ -34,7 +27,6 @@ class Projector(nn.Module):
         super().__init__()
         
         self.linear = nn.Linear(input_dim, hidden_dim)
-        # self.activation = nn.Sigmoid()
         self.activation = nn.SiLU()
         
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -45,12 +37,10 @@ class Decoder(nn.Module):
         super().__init__()
         
         self.linear = nn.Linear(hidden_dim * 2, input_dim)
-        # self.activation = nn.Sigmoid()
         self.activation = nn.SiLU()
         
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.linear(self.activation(x))
-        # return self.activation(self.linear(x))
 
 class SwitchTab(nn.Module):
     def __init__(self,
@@ -62,12 +52,24 @@ class SwitchTab(nn.Module):
                     dropout_rate: float,
                     encoder_depth: int = 3,
                     n_head: int = 2) -> None:
+        """Initialize SwitchTab
+
+        Args:
+            input_dim (int): The dimensionality of the input features.
+            output_dim (int): The dimensionality of the output.
+            category_dims (List[int]): A list of dimensions of the categorical features.
+            hidden_dim (int): The dimensionality of the hidden layers within the network.
+            ffn_factor (int): The scaling factor for the size of the feedforward network inside the encoder.
+            dropout_rate (float): The dropout rate used within the encoder.
+            encoder_depth (int, optional): The number of layers in the encoder. Defaults to 3.
+            n_head (int, optional): The number of attention heads in the encoder. Defaults to 2.
+        """
         super().__init__()
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
         self.__return_salient_feature = False
         
-        # self.tokenizer = MLP(input_dim, hidden_dim, 1, dropout_rate)
+        
         self.encoder = Encoder(input_dim - len(category_dims), category_dims, ffn_factor, hidden_dim, dropout_rate, encoder_depth, n_head)
         self.projector_m = Projector(hidden_dim, hidden_dim)
         self.projector_s = Projector(hidden_dim, hidden_dim)
@@ -82,61 +84,81 @@ class SwitchTab(nn.Module):
         initialize_weights(self.head)
         
     def set_first_phase(self) -> None:
+        """Set first phase step as the forward pass
+        """
         self.forward = self.__first_phase_step
     
     def set_second_phase(self) -> None:
+        """Set second phase step as the forward pass
+        """
         self.forward = self.__second_phase_step
 
     @property
-    def return_salient_feature(self):
+    def return_salient_feature(self) -> bool:
+        """Gets the value of the private attribute `__return_salient_feature` which indicates whether 
+        salient features should be returned by the model.
+
+        Returns:
+            bool: The current state of the '__return_salient_feature' flag.
+        """
         return self.__return_salient_feature
     
     @return_salient_feature.setter
-    def return_salient_feature(self, flag):
+    def return_salient_feature(self, flag: bool) -> None:
+        """Sets the value of the private attribute `__return_salient_feature` to control whether 
+        salient features should be returned by the model.
+
+        Args:
+            flag (bool): A boolean value to set the `__return_salient_feature` attribute.
+        """
         self.__return_salient_feature = flag
         
-    def __first_phase_step(self, xcs: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def __first_phase_step(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """The first phase step of SwitchTab
+        Processes the given samples to decuple salient and mutual embeddings across data samples.
+        
+        Args:
+            x (torch.Tensor): The input batch
 
-        # xs, xcs, ys = batch
-        size = len(xcs) // 2
-        # print(xcs)
-        # emb = self.tokenizer(xcs)
-        # print(emb.shape)
-        # print(emb.shape, emb)
-        zs = self.encoder(xcs)
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: Reconstructed tensors and predicted labels.
+        """
+
+        size = len(x) // 2
+        
+        zs = self.encoder(x)
         ms = self.projector_m(zs)
         ss = self.projector_s(zs)
         
         m1, s1 = ms[:size], ss[:size]
         m2, s2 = ms[size:], ss[size:]
         
-        # m1s1 = torch.concat([m1, s1])
-        # m1s2 = torch.concat([m1, s2])
-        # m2s1 = torch.concat([m2, s1])
-        # m2s2 = torch.concat([m2, s2])
-        # print(m1.shape, s1.shape)
-        # print(torch.concat([m1, s1], dim=1).shape)
         x1_tilde_hat = torch.concat([torch.concat([m1, s1], dim=1), torch.concat([m2, s1], dim=1)])
         x2_hat_tilde = torch.concat([torch.concat([m1, s2], dim=1), torch.concat([m2, s2], dim=1)])
-        # print(x1_tilde_hat.shape, x2_hat_tilde.shape, self.decoder)
-        # print(x1_tilde_hat, x2_hat_tilde)
+
         x1_recover_switch = self.decoder(x1_tilde_hat)
         x2_switch_recover = self.decoder(x2_hat_tilde)
         
         x_hat = torch.concat([x1_recover_switch, x2_switch_recover])
-        # x1_recover = self.decoder(m1s1)
-        # x2_switch = self.decoder(m1s2)
-        # x2_recover = self.decoder(m2s1)
-        # x1_switch = self.decoder(m2s2)
-        
-        # cls_idx = (ys != None)
+
         y_hat = self.head(self.activation(zs))
-        # print("xhat,yhat", zs, x_hat, y_hat)
+
         return x_hat, y_hat
 
     
     def __second_phase_step(self, 
                 x : torch.Tensor) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+        """The second phase step of SwitchTab
+
+        Args:
+            x (torch.Tensor): The input batch
+
+        Returns:
+            Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]: If 'return_salient_feature' is False,
+                returns a tensor 'y_hat' representing the predicted label. If True, returns a tuple
+                where the first element is 'y_hat' and the second element is a tensor of salient features extracted
+                from the input.
+        """
         emb = self.encoder(x)
         y_hat = self.head(self.activation(emb))
         if not self.return_salient_feature:
