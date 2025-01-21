97f4244baf23607338450414cd548c2e93cc5303
Alcoholrithm <kmiiiaa@pusan.ac.kr>
2025-01-21 16:11:54 +0900

    Add hyperparameters of TabularBinning in TabularBinningConfig

--- ts3l/utils/tabularbinning_utils/tabularbinning_config.py
+++ ts3l/utils/tabularbinning_utils/tabularbinning_config.py
@@ -5,4 +5,40 @@ from typing import Any, List, Optional
 
 @dataclass
 class TabularBinningConfig(BaseConfig):
-    pass
+    """
+    Configuration class for initializing components of the TabularBinningLightning Module, including hyperparameters of TabularBinning,
+    optimizers, learning rate schedulers, and loss functions, along with their respective hyperparameters.
+
+    Inherits Attributes:
+        task (str): Specify whether the problem is regression or classification.
+        embedding_config (BaseEmbeddingConfig): Configuration for the embedding layer.
+        backbone_config (BaseBackboneConfig): Configuration for the backbone network.
+        output_dim (int): The dimension of output.
+        loss_fn (str): Name of the loss function to be used. Must be an attribute of 'torch.nn'.
+        loss_hparams (Dict[str, Any]): Hyperparameters for the loss function. Default is empty dictionary.
+        metric (str): Name of the metric to be used. Must be an attribute of 'torchmetrics.functional' or 'sklearn.metrics'. Default is None.
+        metric_hparams (Dict[str, Any]): Hyperparameters for the metric. Default is an empty dictionary.
+        optim (str): Name of the optimizer to be used. Must be an attribute of 'torch.optim'. Default is 'AdamW'.
+        optim_hparams (Dict[str, Any]): Hyperparameters for the optimizer. Default is {'lr': 0.0001, 'weight_decay': 0.00005}.
+        scheduler (str): Name of the learning rate scheduler to be used. Must be an attribute of 'torch.optim.lr_scheduler' or None. Default is None.
+        scheduler_hparams (Dict[str, Any]): Hyperparameters for the scheduler. Default is None, indicating no scheduler is used.
+        initialization (str): The way to initialize neural network parameters. Default is 'kaiming_uniform'.
+        random_seed (int): Seed for random number generators to ensure reproducibility. Defaults to 42.
+    
+     New Attributes:
+        n_bins (int): The number of bins for the pretext task.
+        pretext_loss (str): The loss for the pretext task.
+    Raises:
+        ValueError: Inherited from `BaseConfig` to indicate that a configuration for the task, optimizer, scheduler, loss function, or metric is either invalid or not specified.
+        ValueError: If the specified 'pretext_loss' is not in ["BinRecon", "BinXent"].
+    """
+
+    n_bins: int = field(default=10)
+
+    pretext_loss: str = field(default="BinRecon")
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        if self.pretext_loss not in ["BinRecon", "BinXent"]:
+            raise ValueError('The pretext loss must be one of ["BinRecon", "BinXent"], but %s.' % self.pretext_loss)
